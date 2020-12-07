1af7de01776e43325355b12cc59509cae9c17bae
Claire <a0505kimo@hotmail>
2020-12-07 16:08:32 +0800

    設定App layout佈局畫面，新增一個客制化佈局custom_toast.xml

---
+++ app/src/main/java/com/example/lab6_toast_alertdialog/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab6_toast_alertdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

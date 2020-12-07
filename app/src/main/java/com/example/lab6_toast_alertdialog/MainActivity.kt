6f469cb0502c6e4f86818077858f55afb060f1fe
Claire <a0505kimo@hotmail>
2020-12-07 17:03:44 +0800

    1.在build.gradle啟用ViewBinding buildFeatures{         viewBinding true     } 2.在Activity中使用View Binding 綁定視圖  private lateinit var binding: ActivityMainBinding 3.在onCreate()中使用binding對象訪問視圖 binding = ActivityMainBinding.inflate(layoutInflater)         val view = binding.root         setContentView(view)

--- app/src/main/java/com/example/lab6_toast_alertdialog/MainActivity.kt
+++ app/src/main/java/com/example/lab6_toast_alertdialog/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.lab6_toast_alertdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lab6_toast_alertdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //在Activity中使用View Binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //使用binding對象訪問視圖
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
